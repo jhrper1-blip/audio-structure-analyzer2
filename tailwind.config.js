d0cce4e22bea3787d05c04ef58a3b37cfbfe8913
jhrper1-blip <jackharper087@gmail.com>
2025-08-03 07:21:50 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+    theme: {
+    extend: {
+      colors: {
+        strooq: {
+          primary: '#00D0FF',
+          accent: '#FF00A8',
+          bg: '#0F111A',
+          light: '#F5F7FA'
+        }
+      }
+    }
   },
   plugins: [],
-};
+}
